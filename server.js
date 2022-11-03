227195b31bfda5ad479b6f8394ad459e095f1e28
Ajay Joshi <joshiajay311@gmail.com>
2022-11-03 17:39:02 +0530

    HBS template
    Signed-off-by: Ajay Joshi <joshiajay311@gmail.com>

--- server.js
+++ server.js
@@ -3,38 +3,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
+
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'hbs')
 
 let task = [
     'sample input'
 ]
 
 app.get('/', (req,res)=>{
-    let taskList = task.map(t => `<li>${t}</li>`).join('\n')
-    res.send(
-    `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todo List</title>
-</head>
-<body>
-    <form action="/" method="POST">
-        <input type="text" name="newTask" id="input">
-        <button type="submit">Add</button>
-        <ul>
-            ${taskList}
-        </ul>
-    </form>
-</body>
-</html>`
-)
+    res.render('home', {
+        Title: 'Task Manager' , 
+        task
+    })
 })
 
 app.post('/', (req,res)=>{
-
     task.push(req.body.newTask)
     res.redirect('/')
 })
